25bf4d2c4b5d32d1a6e78022796b0bf1e44bfd53
Simon Bond <sjb277@medschl.cam.ac.uk>
2018-12-21 13:35:47 +0000

    error in doc

--- R/cctu_initialise.R
+++ R/cctu_initialise.R
@@ -2,7 +2,7 @@
 #'
 #' @param root the root directory to start in
 #' @param scripts logical create the standard set of scripts. Intended to be used once interactively at the start of coding for an analysis.
-#' @param rm logical whether to also run \code{\link{rm_ouput}} with its default values, to delete all the files in teh output directory.
+#' @param rm logical whether to also run \code{\link{rm_output}} with its default values, to delete all the files in teh output directory.
 #' @return cctu_initialise gives an invisible return of logical indicating if the directories have been created. The directories needed are "Output", and within "Output", "Core", "Figures", "Reports".
 #'
 #' @seealso \code{\link{dir.create}}

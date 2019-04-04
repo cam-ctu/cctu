9fa1600458bce9a0972db551dbf8207130fb3140
Simon Bond <sjb277@medschl.cam.ac.uk>
2019-04-04 12:18:59 +0100

    another go with travis tests

--- tests/testthat/test_run_batch.R
+++ tests/testthat/test_run_batch.R
@@ -23,7 +23,7 @@ test_that("non windows",
             skip_if(Sys.info()["sysname"] == "Windows", "Only run for non-windows OS")
             expect_warning(run_batch("script_to_test_run_batch.R"),
                            "run_batch\\(\\) only works in interactive mode")
-            return_text <- system("R --interactive < container.R ", intern=TRUE)
+            return_text <- system("$(R_HOME)/bin/R --interactive < container.R ", intern=TRUE)
             expect_match(paste(return_text,collapse="\n"),"run_batch\\(\\) only works in Windows")
           }
 )

08bc7e36c66e2a552dc379253f349e19bea8271c
Alimu Dayimu <alimdayim@hotmail.com>
2022-03-17 22:31:05 +0000

    Macro database related functions

---
+++ R/macro_utils.R
@@ -0,0 +1,171 @@
+
+#' Apply DLU/CLU file to the data frame
+#'
+#' The variable label attribute will be applied to the data frame from the DLU file.
+#'
+#' @param data Data frame to be applied.
+#' @param dlu Data frame of DLU
+#' @param clu Data frame of CLU
+#' @param date_format Date format to be converted, default is `\%d/\%m/\%Y`.
+#' @details This funciton first convert the data to a \code{\link[data.table]{data.table}}.
+#' This is to avoid the variable attributes dropped by base R functions. Then it will use
+#' the dlu file to convert the data into corresponding variable types.
+#' \itemize{
+#'   \item{IntegerData}: Convert to numeric.
+#'   \item{Real}: Convert to numeric.
+#'   \item{Category}: If there are any non-numerica characters in the variable, no conversion
+#' will be performed, otherwise convert to numeric.
+#'   \item{Date}: Convert data date format with \code{\link{as.Date}}. The \code{date_format}
+#' will be used to feed the \code{\link{as.Date}} function during the covnersion.
+#'   \item{Text}: Convert to character.
+#' }
+#' After the conversion of the data, variable label attribute will be created for the variable.
+#' See  \code{\link{var_lab}}.
+#' 
+#' After this is completed and if the clu file is provided, value label attribute will be 
+#' create for the variables listed in the clu file. See \code{\link{val_lab}}.
+#' @seealso \code{\link{var_lab}} \code{\link{val_lab}} \code{\link{sep_dlu}}
+#'  \code{\link[data.table]{data.table}}
+#' @return A data.table object.
+#' @export
+#'
+apply_lus <- function(data, dlu, clu = NULL, date_format = "%d/%m/%Y"){
+
+  data.table::setDT(data)
+
+  if(ncol(dlu) == 4 & names(dlu)[2] == "Visit.Form.Question")
+    dlu <- sep_dlu(dlu)
+
+  # Store DLU file inside the cctu env
+  cctu_env$dlu <- dlu
+
+  # Keep the variables in the data only
+  dlu <- dlu[dlu$ShortCode %in% names(data), ]
+
+  for (i in dlu$ShortCode) {
+    # Format date
+    if (dlu$Type[dlu$ShortCode == i] == "Date")
+      data[[i]] <- as.Date(data[[i]], date_format)
+
+    # Format Number
+    if (dlu$Type[dlu$ShortCode == i] %in% c("IntegerData", "Real", "Category")){
+      # Skip the variables with characters
+      if(!any(grepl("\\D+", data[[i]])))
+        data[[i]] <- as.numeric(data[[i]])
+    }
+
+    # Format date
+    if (dlu$Type[dlu$ShortCode == i] == "Text")
+      data[[i]] <- as.character(data[[i]])
+
+    # Assign label
+    var_lab(data[[i]]) <- dlu$Description[dlu$ShortCode == i]
+
+    if(!is.null(clu) && i %in% clu$ShortCode){
+      valab <- clu[clu$ShortCode == i, "CatCode"]
+      names(valab) <- clu[clu$ShortCode == i, "CatValue"]
+      val_lab(data[[i]]) <- valab
+    }
+
+  }
+
+  return(data)
+}
+
+
+
+#' Tidy DLU form
+#'
+#' @description Separate Visit, Form and Question into different columns.
+#' The DLU file contains four columns: 
+#' \itemize{
+#'   \item{ShortCode}: Variable name of the downloaded data.
+#'   \item{Visit/Form/Question}: Contains visit, form and question. It is
+#' separated by a slash. This function will separate this column into `Visit`,
+#' `Form` and `Question` column in the output dataset. The `Question` column
+#' is the unique variable name for a particular CRF form.
+#'   \item{Description}: Description of the variable, namely variable label.
+#' This is will be used by \code{\link{apply_lus}} to create variable label.
+#'  \item{Type}: Type of the variable, it has IntegerData, Text, Date, Real and 
+#' Category four categories. This will be used by \code{\link{apply_lus}} to
+#' convert variables to corresponding type.
+#' }
+#' @param x DLU data.frame
+#' @return A data.frame
+#' @seealso \code{\link{apply_lus}} 
+#' @export
+#'
+sep_dlu <- function(x){
+  vfq <- strsplit(as.character(x$Visit.Form.Question),'/')
+  vfq <- as.data.frame(do.call(rbind, vfq))
+  colnames(vfq) <- c("Visit", "Form", "Question")
+  cbind.data.frame(x[, -2], vfq)
+}
+
+#' Extract data by form from MACRO dataset
+#'
+#' @description Extract data by form from MACRO dataset. Data will be transformed to long format
+#'  adding a new column of `Visit`. 
+#'
+#' @param data A data.frame from macro dataset.
+#' @param form Name of the form in the DLU file, see \code{\link{sep_dlu}}.
+#' @param dlu A DLU data.frame
+#' @param visit A character string or vector of visit name in the DLU file, see \code{\link{sep_dlu}}.
+#' @param vars_keep Parameters to keep in the output data. This is useful if you want to keep
+#' treatment arm or age variable.
+#' @seealso \code{\link{sep_dlu}} \code{\link[data.table]{data.table}}
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' extract_form(full_dt, "LabResF", dlu)
+#' }
+#' 
+#' @return A data.table object.
+#'
+#' @importFrom stats setNames
+#'
+#'
+extract_form <- function(data, form, visit = NULL, vars_keep = NULL, dlu = cctu_env$dlu){
+  if(ncol(dlu) == 4 & names(dlu)[2] == "Visit.Form.Question")
+    dlu <- sep_dlu(dlu)
+
+  if(length(form) > 1)
+    stop("Form must be of length 1.")
+
+  if(!form %in% dlu$Form)
+    stop("Form name ", form, " can not be found in the DLU file.")
+
+  dlu <- dlu[dlu$Form == form, ]
+
+  if(!is.null(visit)){
+    if(!all(visit %in% dlu$Visit))
+      stop("Visit name ", paste(setdiff(visit, dlu$Visit), collapse = ", "), " can not be found in the DLU file.")
+      
+    dlu <- dlu[dlu$Visit %in% visit, ]
+  }
+    
+
+  data.table::setDT(data)
+
+  res <- lapply(unique(dlu$Visit), function(v){
+    vars_selc <- dlu$ShortCode[dlu$Visit == v]
+    dt <- data[, c(vars_keep, vars_selc), with = FALSE]
+    colnames(dt) <- c(vars_keep, dlu$Question[dlu$Visit == v])
+
+    # If the number of missing column equals to the number of variables
+    dt$if.all.miss <- rowSums(apply(dt[, dlu$Question[dlu$Visit == v], with = FALSE],
+                                    2,
+                             function(x)x %in% c(NA, ""))) == length(vars_selc)
+    dt$FormVisit <- v
+    dt
+  })
+
+  res <- data.table::rbindlist(res, use.names=TRUE)
+  res <- res[!res$if.all.miss, ]
+  res$if.all.miss <- NULL
+
+  return(data.table::setDT(res))
+}
+
+
---
+++ tests/testthat/test-macro_utils.R
@@ -0,0 +1,49 @@
+# Test MACRO util functions
+
+dt <- read.csv(system.file("extdata", "pilotdata.csv", package="cctu"))
+dlu <- read.csv(system.file("extdata", "pilotdata_dlu.csv", package="cctu"))
+clu <- read.csv(system.file("extdata", "pilotdata_clu.csv", package="cctu"))
+
+# dt <- read.csv("inst/extdata/pilotdata.csv")
+# dlu <- read.csv("inst/extdata/pilotdata_dlu.csv")
+# clu <- read.csv("inst/extdata/pilotdata_clu.csv")
+
+dt$subjid <- substr(dt$USUBJID, 8, 11)
+
+test_that("Apply DLU and CLU files", {
+  dt <- apply_lus(dt, dlu, clu)
+  expect_s3_class(dt, "data.table")
+  expect_identical(var_lab(dt$ARM), "Treatment Arm")
+  expect_identical(val_lab(dt$ARM), c("Placebo" = 1L, "Research" = 2L))
+
+  expect_equal(nrow(cctu_env$dlu), nrow(dlu))
+  expect_identical(names(cctu_env$dlu), c("ShortCode", "Description", "Type",
+                                          "Visit", "Form", "Question"))
+
+})
+
+
+test_that("Extract form", {
+  dt <- apply_lus(dt, dlu, clu)
+  lb <- extract_form(dt, "Lab")
+  expect_true(all(unique(lb$FormVisit) %in% c("SCREENING", "TRT", "ENDTRT")))
+
+  expect_true(all(levels(to_factor(lb$AVISIT)) %in% c('Baseline','Week 4','Week 8','Week 16','End of Treatment')))
+
+  lb <- extract_form(dt, "Lab", visit = "SCREENING")
+  expect_identical(unique(lb$FormVisit), "SCREENING")
+
+  expect_error(extract_form(dt, "SCREENING"),
+               "Form name SCREENING can not be found in the DLU file")
+
+  expect_error(extract_form(dt, c("PatientReg", "Lab")),
+               "Form must be of length 1")
+
+  lb <- extract_form(dt, "Lab", visit = c("SCREENING", "TRT"))
+  expect_true(all(unique(lb$FormVisit) %in% c("SCREENING", "TRT")))
+
+  expect_error(extract_form(dt, "Lab", visit = c("SCREENING", "TEST")),
+               "Visit name TEST can not be found in the DLU file")
+
+})
+

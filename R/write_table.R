21fc903d1272bf9910e4cdab7aeac8f25b6f993b
Alimu Dayimu <alimdayim@hotmail.com>
2022-03-18 12:09:07 +0000

    Write table match new xslt file and cttab class

--- R/write_table.R
+++ R/write_table.R
@@ -1,4 +1,5 @@
-#' Function to write a table into xml format in the correct directory, and edit TableofTables
+#' Function to write a table into xml format in the correct directory, and edit
+#' TableofTables
 #'
 #' @inheritParams write_ggplot
 #' @param X the data.frame or table to be saved in xml format
@@ -23,57 +24,21 @@ write_table = function(X,
 
   CallingProg <- cctu_env$parent[1]  #get_file_name()
   if(is.null(CallingProg)){
-    warning(paste("Unable to identify the code file that created table", number))
+    warning("Unable to identify the code file that created table", number)
     CallingProg <- "Missing"
-    }
+  }
   add_program(number, CallingProg)
 
-
-  output_string <- NULL
-
-  paste_plus <- function(..., initial = output_string){
-    arg_name <- deparse(substitute(initial))
-    initial  <- paste(initial, ...)
-    assign(arg_name, initial, envir = parent.frame())
-  }
-  if( inherits(X,c("tbl_df","matrix","array"))){X <- as.data.frame(X)}
-  utf8_check <- detect_invalid_utf8(X)
-  if( nrow(utf8_check)){
-    warning("Invalid non-UTF8 characters found\n", utf8_check,
-            "\nRemove manually from the xml output, or clean input data at source,
-   or clean using remove__invalid_utf8()")
-  }
-
-
-  paste_plus("<table>\n<tr>")
-  for(col in 1:dim(X)[2]){
-    #clean the heading here
-    heading[col] <- remove_xml_specials(heading[col])
-    paste_plus("<td>", heading[col], "</td>")
-  }
-  paste_plus("</tr>\n")
-  for(row in 1:dim(X)[1]){
-    paste_plus("<tr>")
-    for(col in 1:dim(X)[2]){
-      text <-  as.character(X[row, col])
-      if(na_to_empty){  text <- ifelse(is.na(text),"", text) }
-      text <- remove_xml_specials(text)
-      paste_plus("<td>", text, "</td>")
-
-    }
-    paste_plus("</tr>\n")
-  }
-  paste_plus("</table>\n")
-
+  if(inherits(X, "cttab"))
+    output_string <- table_cttab(X)
+  else
+    output_string <- table_data(X, heading, na_to_empty)
 
   #directory %<>% normalizePath %>% final_slash
   file_name <- file.path(directory,paste0("table_",number,".xml"))
 
   cat(output_string, file = file_name, append = FALSE)
   if(verbose){cat("\n", file_name, "created.\n")}
-
-
-
 
   if(clean_up){
      clean_up(number, frame = parent.frame(), verbose=verbose)
@@ -83,7 +48,6 @@ write_table = function(X,
 
 #' @keywords internal
 #'
-
 remove_xml_specials <- function(x){
   x <- gsub("&(?!#\\d+;)","&amp;\\1", x,perl=TRUE)
   x <-  gsub("<","&lt;", x)
@@ -93,6 +57,115 @@ remove_xml_specials <- function(x){
   x
 }
 
+
+
+# For normal
+#' @keywords internal
+#'
+table_data <- function(X, heading  = colnames(X), na_to_empty=FALSE){
+
+  check <- as.character(rownames(X)) != as.character(1:nrow(X))
+  if(inherits(X, "matrix") & any(check)){
+    heading <- c("Variables", heading)
+    X <- data.frame(row_nam = rownames(X), X, row.names = NULL)
+  }
+
+  # Variable names to labels if no variable label
+  with_varlab <- sapply(X, has.label)
+  if(any(with_varlab)){
+    heading[with_varlab] <- unlist(var_lab(X)[with_varlab])
+  }
+
+  # Variable values to labels if has value
+  X <- lab2val(X)
+
+  if(inherits(X, "data.frame")){
+    utf8_check <- detect_invalid_utf8(X)
+    if( nrow(utf8_check)){
+      warning("Invalid non-UTF8 characters found\n", utf8_check,
+              "\nRemove manually from the xml output, or clean input data at source,
+    or clean using remove__invalid_utf8()")
+    }
+  }
+  
+
+  # Table header
+  th <- paste0("<th>", remove_xml_specials(heading), "</th>", collapse = "")
+  th <- paste0("<tr>", th, "</tr>\n")
+  thead <- paste0("<thead>\n", th, "</thead>\n")
+
+  # Table body
+  td <- apply(X, 2, function(c){
+    if(na_to_empty)
+      c <- ifelse(is.na(c),"", c)
+    paste0("<td>", remove_xml_specials(c), "</td>")
+  })
+  if(is.null(dim(td)))
+    td <- paste0(td, collapse = "")
+  else
+    td <- apply(td, 1, paste0, collapse="")
+  td <- paste0("<tr>", td, "</tr>\n", collapse="")
+  tbody <- paste0("<tbody>\n", td, "</tbody>\n")
+
+  # Table output
+  paste("\n<table>\n", thead, tbody, "</table>\n")
+}
+
+
+# For cttab class
+#' @keywords internal
+#'
+table_cttab <- function(x) {
+
+  rl <- rownames(x)
+  rowclass <- attr(x, "position")
+
+  if(inherits(x, "matrix")){
+    x <- cbind("Variable" = rl, x)
+    al <- "firstleft"
+    hd_nam <- colnames(x)
+  }else{
+    # Variable names to labels if no variable label
+    with_varlab <- sapply(x, has.label)
+    for (i in names(x)[!with_varlab]) {
+      var_lab(x[[i]]) <- i
+    }
+    hd_nam <- unlist(var_lab(x))
+    # Variable values to labels if has value
+    x <- lab2val(x)
+    al <- ""
+  }
+
+  x <- apply(x, 2, remove_xml_specials)
+
+  # Table header
+  th <- paste0("<th>", remove_xml_specials(hd_nam), "</th>", collapse = "")
+  th <- paste0("<tr>", th, "</tr>\n")
+  thead <- paste0("<thead>\n", th, "</thead>\n")
+
+  # Table body
+  # Add class
+  cls <- matrix(NA, nrow(x), ncol(x))
+  rowclass <- sapply(seq_along(rowclass),
+                     function(x) switch(as.character(rowclass[x]),
+                                        "0" = "bold;bgcol;span",
+                                        "1" = "bold",
+                                        "2" = "bold;span",
+                                        "3" = "indent",
+                                        ""))
+
+  cls[,1] <- paste0(" style='", paste(rowclass, al, sep = ";"), "'")
+  cls[is.na(cls)] <- ""
+
+  td <- paste0("<td", cls, ">", x, "</td>")
+  dim(td) <- dim(x)
+  td <- apply(td, 1, paste0, collapse="")
+  td <- paste0("<tr>", td, "</tr>\n", collapse="")
+  tbody <- paste0("<tbody>\n", td, "</tbody>\n")
+
+  # Table output
+  paste("\n<table>\n", thead, tbody, "</table>\n")
+}
 
 
 

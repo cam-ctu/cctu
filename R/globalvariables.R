94792055b94ae704ca287f8f74b7dd6cc84bd5e3
Alimu Dayimu <alimdayim@hotmail.com>
2022-03-18 12:13:05 +0000

    Initialise missing report and dlu data

--- R/globalvariables.R
+++ R/globalvariables.R
@@ -6,5 +6,13 @@ cctu_env$number <- "0"
 cctu_env$sumby_count <- 0
 cctu_env$nested_run_batch <- FALSE
 
+# Missing data report
+cctu_env$missing_report_data <- setNames(data.frame(matrix(ncol = 8, nrow = 0)),
+                                         c("form", "visit_var", "visit_label", 
+                                           "visit", "variable", "label", "missing_pct",
+                                           "subject_ID"))
+# Setup DLU file                                           
+cctu_env$dlu <- NULL
+
 #.reserved <- character(0)
 

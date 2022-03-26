69b3000637226377161cfb51d9edf2f5221e076f
Alimu Dayimu <alimdayim@hotmail.com>
2022-03-26 15:27:48 +0000

    Test if variable is numeric

--- R/utils.R
+++ R/utils.R
@@ -30,3 +30,29 @@ rbind.cttab <- function(...){
 }
 
 
+#' Check if All Elements in Character Vector are Numeric
+#'
+#' Tests, without issuing warnings, whether all elements of a character
+#' vector are legal numeric values, or optionally converts the vector to
+#'  a numeric vector. Leading and trailing blanks in x are ignored.
+#'
+#' @param x a character vector
+#' @param extras a vector of character strings to count as numeric values,
+#' other than "".
+#'
+#' @return a logical value
+#'
+#' @examples
+#' all_is_numeric(c('1','1.2','3'))
+#' all_is_numeric(c('1','1.2','3a'))
+#'
+#' @keywords internal
+
+all_is_numeric <- function(x, extras=c('.','NA')){
+  x <- sub('[[:space:]]+$', '', x)
+  x <- sub('^[[:space:]]+', '', x)
+  xs <- x[!x %in% c('',extras)]
+  if(!length(xs))
+    FALSE
+  suppressWarnings(!any(is.na(as.numeric(xs))))
+}

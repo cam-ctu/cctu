c9bd15a60834ebfe40ff7bc5f764a27461a05b95
Alimu Dayimu <alimdayim@hotmail.com>
2023-04-24 20:30:13 +0100

    Detect non-UTF-8 characters for matrix as well

--- R/utf8.R
+++ R/utf8.R
@@ -1,8 +1,8 @@
 #' Functions to detect and delete non-UTF8 characters, which the XML output will not like
 #'
-#' @param data  a data.frame, typically the input to \code{write_table}
+#' @param data  a data.frame or a matrix, typically the input to \code{write_table}
 #'
-#' @return \code{detect_invalid_utf8} returns a data.frame that gives the column, row,
+#' @return \code{detect_invalid_utf8} returns a data.frame  or a matrix that gives the column, row,
 #' and value of any dataum points that contain invalid characters, or a zero-length
 #' data frame if none are present.  \code{remove_invalid_utf8} replaces any invalid characters
 #' with an empty string.
@@ -11,7 +11,7 @@
 #' @export
 
 detect_invalid_utf8 <- function(data){
-  if( !inherits(data, "data.frame")){stop("input a data.frame")}
+  if( !inherits(data, c("data.frame", "matrix"))){stop("input a data.frame or matrix")}
   X <- lapply(data,invalid_utf8_)
   where <- lapply(X, which)
   col_names <- names(where)
@@ -26,7 +26,7 @@ detect_invalid_utf8 <- function(data){
 #' @describeIn detect_invalid_utf8 Function to delete invalid UTF8 charachters
 #' @export
 remove_invalid_utf8 <- function(data){
-  if( !inherits(data, "data.frame")){stop("input a data.frame")}
+  if( !inherits(data, c("data.frame", "matrix"))){stop("input a data.frame or matrix")}
   X <- lapply(data, function(x){ iconv(x, "UTF-8", "UTF-8",sub='')})
   X <- lapply(X, function(x){gsub("\u00A0","\u0020",x)})
   data.frame(X)

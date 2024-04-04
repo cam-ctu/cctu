c97f1eaa6c209a853a2f68c4662a3d26c1e15f65
shug0131 <shug0131@gmail.com>
2024-04-04 15:59:27 +0100

    extra classes

--- R/regression_table.R
+++ R/regression_table.R
@@ -11,12 +11,41 @@ covar <- function(x,...){
 
 coef_table.lm <- function(x,level=0.95, ...){
   output <- cbind(
-    summary(x)$coefficients[,c(1,2,4)],
+    summary(x)$coefficients[,c(1,2,4), drop=FALSE],
     confint(x, level=level)
   )
   colnames(output) <- c("beta","se","p","lower","upper")
   output
 }
+
+coef_table.coxph <- function(x,level=0.95, ...){
+  output <- cbind(
+    summary(x)$coefficients[,c(1,3,5), drop=FALSE],
+    confint(x, level=level)
+  )
+  colnames(output) <- c("beta","se","p","lower","upper")
+  output
+}
+
+coef_table.lme <- function(x,level=0.95, ...){
+  output <- cbind(
+    summary(x)$tTable[,c(1,2,5), drop=FALSE],
+    intervals(x, level=level,which="fixed")$fixed[,c(1,3)]
+  )
+  colnames(output) <- c("beta","se","p","lower","upper")
+  output
+}
+
+coef_table.gls <- function(x, level=0.95,...){
+  output <- cbind(
+    summary(x)$tTable[,c(1,2,4), drop=FALSE],
+    intervals(x, level=level, which="coef")$coef[,c(1,3)]
+  )
+  colnames(output) <- c("beta","se","p","lower","upper")
+  output
+}
+
+
 
 covar.lm <- function(x,digits=3,...){
   x_summary <- summary(x)
@@ -42,14 +71,40 @@ covar.glm <- function(x,digits=3,...){
   output
 }
 
+covar.coxph <- function(x,...){
+  output <- cbind(
+    contrasts=c("Number of Observations","Number of Events"),
+    beta=c(x$n, x$nevent)
+    )
+  output
+  }
 
+covar.lme <- function(x, digits=3,...){
+    X <- VarCorr(x)
+    X <- rbind(colnames(X), X)
+    # Want to apply formatting to numbers
+    index <- grep("\\d+\\.\\d+", X)
+    X[index] <- X[index] %>% as.numeric %>% signif_pad(digits=digits)
 
+    n <- x$dims$N
+    n_groups <- x$dims$ngrps[1:x$dims$Q]
+    contrasts <- c("Random Effects", rownames(X)[-1],"Number of Observations",names(n_groups))
+    cols <- ncol(X)
+    empty <- list("")
+    Y <- do.call(cbind, c(list(c(n,n_groups)), empty[rep(1,cols-1)]))
+    output <- cbind(contrasts, rbind(X,Y))
+    output
+  }
 
-
+covar.gls <- function(x, digits=3, ...){
+  data.frame(contrasts=c("Number of Observations", "Residual SE"),
+            beta=c(x$dims$N, signif_pad(x$sigma, digits=digits))
+  )
+}
 
 regression_table <- function(x,labels=names(coef(x)),
                              digits=3,p_digits=4,
-                             trans=if(class(x)[1]=="lm"){NULL}else{exp},
+                             trans=if(class(x)[1] %in% c("glm","coxph")){exp}else{NULL},
                              level=0.95,
                              col_names=guess_col_names(x,trans)
                              ){
@@ -71,15 +126,21 @@ regression_table <- function(x,labels=names(coef(x)),
   X <- data.frame(do.call(cbind, var_list))
   # Needs to be more general to deal with extra transformed columns
   empty <- list("")
+  Z <- covar(x, digits=digits)
   Y <- as.data.frame(
     do.call( cbind,
-             c(list(covar(x, digits=digits)),
-               empty[rep(1, ncol(X)-2)])
+             c(list(Z),
+               empty[rep(1, ncol(X)-ncol(Z))])
     )
   )
   names(Y) <- names(X)
   X <- rbind_space(X, Y)
   names(X) <- col_names
+  if( inherits(x,c("gls","lme"))){warning(
+  "The variance-correlation structure is not included as it is too general
+  consider summary(x), coef(x$modelStruct$corStruct, uncons=FALSE),
+  or coef(x$modelStruct$varStruct, uncons=FALSE)")
+                        }
   X
 }
 
@@ -116,6 +177,8 @@ guess_col_names.coxph <- function(x,trans,...){
 
 
 library(MASS)
+library(survival)
+library(tidyverse)
 data(cats)
 fit_lm <- lm(Hwt~Sex/Bwt-1, data=cats)
 
@@ -128,6 +191,39 @@ regression_table(fit_lm,
 
 data("birthwt")
 head(birthwt)
-fit_glm <- glm(low~. -bwt , family=gaussian, data=birthwt)
-regression_table(fit_glm, trans=NULL)
+fit_glm <- glm(low~. -bwt , family=binomial, data=birthwt)
+regression_table(fit_glm)
+
+blad1 <- bladder %>% #filter(enum==1) %>%
+  mutate(rx= factor(rx, levels=1:2, labels=c("placebo","thiotepa")),
+         enum=factor(enum)
+  )
+head(blad1)
+summary(blad1)
+
+fit_cox <- coxph(Surv(stop, event)~ enum/(rx+size+number)+cluster(id), data=blad1)
+regression_table(fit_cox)
+
+library(nlme)
+Orthodont
+
+fit_lme <- lme(distance~age, random=~1|Sex/Subject, data=Orthodont)
+regression_table(fit_lme)
+
+fit_lme <- lme(distance~age, random=~age|Subject, data=Orthodont,
+               weights=varIdent(form=~1|Sex)
+               )
+summary(fit_lme)
+regression_table(fit_lme)
+
+
+fit_gls <- gls(distance~age, correlation=corCompSymm(form=~1|Subject), data=Orthodont,
+               weights=varIdent(form=~1|Sex)
+               )
+
+regression_table(fit_gls)
+
+
+# GEE and lmer ,  survreg is rarely used ?
+
 

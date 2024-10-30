31ea201a2bc32c3e8b17ac2d5ce835ba6fefd662
shug0131 <sjb277@medschl.cam.ac.uk>
2024-10-30 17:20:11 +0000

    Update source.R

--- R/source.R
+++ R/source.R
@@ -36,40 +36,40 @@ source <- function(file,
 }
 
 
-my_source <- function(file,
-                   local=getOption("cctu_source_local", default = FALSE),
-                   backup=NULL,
-                   toplevel.env = NULL
-){
-  envir <- if (isTRUE(local))
-    parent.frame()
-  else if (isFALSE(local))
-    .GlobalEnv
-  else if (is.environment(local))
-    local
-  else stop("'local' must be TRUE, FALSE or an environment")
-  if(is.null(toplevel.env)) toplevel.env = as.environment(envir)
-  #parent_frame <- parent.frame()
-  if(!is.null(backup)){save.image(file=backup)}
-  if(!exists("code_tree",envir=cctu_env)){
-    warning("Recommend using cctu_initialise()")
-    reset_code_tree(root_file=file.path(getwd(),"ROOT"))
-  }
-  code_tree <- cctu_env$code_tree
-  child <- normalizePath(file)
-  code_tree[1+nrow(code_tree),] <- c(cctu_env$parent[1], child)
-  cctu_env$code_tree <- code_tree
-  cctu_env$parent <- c(child, cctu_env$parent)
-  base_source <- base::source
-  #this function name will still match "source" in a regular expression used in get_file_name
-  #evalq( call( "base_source", file=file, echo=TRUE, max.deparse.length=Inf,local=local), envir=parent_frame)
-  # evalq works but eval does not...
-  sys.source( file=file, envir=envir,
-              #echo=TRUE, max.deparse.length=Inf,
-   #           keep.parse.data=TRUE,
-              toplevel.env=toplevel.env)
-  #print(getParseData())
-  cctu_env$parent <- cctu_env$parent[-1]
-}
+# my_source <- function(file,
+#                    local=getOption("cctu_source_local", default = FALSE),
+#                    backup=NULL,
+#                    toplevel.env = NULL
+# ){
+#   envir <- if (isTRUE(local))
+#     parent.frame()
+#   else if (isFALSE(local))
+#     .GlobalEnv
+#   else if (is.environment(local))
+#     local
+#   else stop("'local' must be TRUE, FALSE or an environment")
+#   if(is.null(toplevel.env)) toplevel.env = as.environment(envir)
+#   #parent_frame <- parent.frame()
+#   if(!is.null(backup)){save.image(file=backup)}
+#   if(!exists("code_tree",envir=cctu_env)){
+#     warning("Recommend using cctu_initialise()")
+#     reset_code_tree(root_file=file.path(getwd(),"ROOT"))
+#   }
+#   code_tree <- cctu_env$code_tree
+#   child <- normalizePath(file)
+#   code_tree[1+nrow(code_tree),] <- c(cctu_env$parent[1], child)
+#   cctu_env$code_tree <- code_tree
+#   cctu_env$parent <- c(child, cctu_env$parent)
+#   base_source <- base::source
+#   #this function name will still match "source" in a regular expression used in get_file_name
+#   #evalq( call( "base_source", file=file, echo=TRUE, max.deparse.length=Inf,local=local), envir=parent_frame)
+#   # evalq works but eval does not...
+#   sys.source( file=file, envir=envir,
+#               #echo=TRUE, max.deparse.length=Inf,
+#    #           keep.parse.data=TRUE,
+#               toplevel.env=toplevel.env)
+#   #print(getParseData())
+#   cctu_env$parent <- cctu_env$parent[-1]
+# }
 
 

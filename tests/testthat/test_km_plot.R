428f76cc98c6e248385604896fa9c52642978d1c
shug0131 <sjb277@medschl.cam.ac.uk>
2025-01-10 16:48:31 +0000

    plan sketched for km plots testing

---
+++ tests/testthat/test_km_plot.R
@@ -0,0 +1,12 @@
+# change name of function to somethign more compact
+
+# test 2 arm, 1 arm.  stratified
+
+# check saved output into a report
+
+# vary the arguments
+
+# test how to edit with patchwork
+
+# compare to eudract work to see if improvements or can copy tests.
+

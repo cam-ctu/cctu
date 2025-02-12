4a7c13a706c955443f04b7442dc3a6b5d5c663f0
Alimu Dayimu <alimdayim@hotmail.com>
2025-02-12 23:12:47 +0000

    Pass any parameters to source to make sure Rstudio source button works.

--- R/source.R
+++ R/source.R
@@ -6,6 +6,7 @@
 #' @param backup A character string giving the file path to which to save an
 #' image of the R environment before sourcing any code.
 #' Default is NULL, which does not save any image.
+#' @param ... other parameters to be passed to \code{\link[base]{source}}.
 #' @details evaluates code in the file argument, and also augments or creates a
 #'  dataframe with two variables (parent, child). The intention is to record
 #'  the architecture of a sequenece of code files run using nested source()
@@ -18,7 +19,11 @@
 
 source <- function(file,
                    local = getOption("cctu_source_local", default = FALSE),
-                   backup = NULL) {
+                   backup = NULL,
+                   ...) {
+
+  dot_args <- list(...)
+
   parent_frame <- parent.frame()
   if (!is.null(backup)) {
     save.image(file = backup)
@@ -33,17 +38,17 @@ source <- function(file,
   cctu_env$code_tree <- code_tree
   cctu_env$parent <- c(child, cctu_env$parent)
   base_source <- base::source
+
+  # Update/provide parameters
+  dot_args$file <- file
+  dot_args$echo <- TRUE
+  dot_args$max.deparse.length <- Inf
+  dot_args$local <- local
+
   # this function name will still match "source" in a regular expression used
   # in get_file_name evalq( call( "base_source", file=file, echo=TRUE,
   # max.deparse.length=Inf,local=local), envir=parent_frame)
   # evalq works but eval does not...
-  do.call(base_source, list(
-    file = file,
-    echo = TRUE,
-    max.deparse.length = Inf,
-    local = local
-  ),
-  envir = parent_frame
-  )
+  do.call(base_source, dot_args, envir = parent_frame)
   cctu_env$parent <- cctu_env$parent[-1]
 }
